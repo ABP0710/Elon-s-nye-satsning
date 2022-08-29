48aff7ac75322dc65263a0c0c346cef20cf6b35b
ABP0710 <110819363+ABP0710@users.noreply.github.com>
2022-08-29 16:00:09 +0200

    Add project files.

---
+++ Elon's nye satsning/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Elon_s_nye_satsning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //school assignment on classes 
+            //make 2 new objects from the RadioControldCar class, and sets the colors to a green car and a blue car
+            RadioControldCar greenCar = new RadioControldCar();
+            RadioControldCar blueCar = new RadioControldCar();
+
+            greenCar.Color = "Green";
+            blueCar.Color = "Blue";
+
+            //charge the green car and sets the distance to drive
+            greenCar.Charge();
+            greenCar.DistanceDriven = 80;
+
+
+            //make the start visual in the console
+            Console.WriteLine("xxxxxxxxxxxxxxxxxx");
+            Console.WriteLine();
+            Console.WriteLine("Green car GOOOOOO!");
+            Console.WriteLine();
+            Console.WriteLine("xxxxxxxxxxxxxxxxxx");
+
+            //run the bool method drive on the green car
+            if(!greenCar.Drive())
+            {
+
+            }
+
+
+            //charge the blue car and sets the distance to drive
+            blueCar.Charge();
+            blueCar.DistanceDriven = 200;
+
+            //make the start visual in the console
+            Console.WriteLine("xxxxxxxxxxxxxxxxxx");
+            Console.WriteLine();
+            Console.WriteLine("Blue car GOOOOOO!");
+            Console.WriteLine();
+            Console.WriteLine("xxxxxxxxxxxxxxxxxx");
+            if (!blueCar.Drive())
+            {
+
+            }
+        }
+    }
+}
---
+++ Elon's nye satsning/RadioControldCar.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Elon_s_nye_satsning
+{
+    internal class RadioControldCar
+    {
+        //instans variable for the RadioControldCar class
+
+        private string _color;
+
+        private double _battery;
+
+        private double _batteryProcentPerMeter;
+
+        private int _distanceDriven;
+
+        //encapsulation 
+
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public double Battery
+        {
+            get { return _battery; }
+            set { _battery = value; }
+        }
+
+        public double BatteryProcentPerMeter
+        {
+            get { return _batteryProcentPerMeter; }
+            set { _batteryProcentPerMeter = value; }
+        }
+
+        public int DistanceDriven
+        {
+            get { return _distanceDriven; }
+            set { _distanceDriven = value; }
+        }
+
+        //methood used for the class
+        //a bool methood, whit the bool canDrive variable inside
+        //if the car has more than 0% power, it will drive
+        //for every meters driven the battery lowers 0.05%
+        //will round the number to 2 digits
+        //the thread.sleep is to make it feel like the car is using the batery
+        //when the battery reaching 10% power, a low power warning is issued
+        //if the battery comes down to 0, the forloop brakes, and tell the user the no power, cant drive!
+        public bool Drive()
+        {
+            bool canDrive = true;
+            if (Battery > 0)
+            {
+                for (int i = 0; i < DistanceDriven; i++)
+                {
+                    ShowDisplay(i);
+                    Battery -= 0.05;
+                    Battery = Math.Round(Battery, 2);
+                    Thread.Sleep(10);
+                    if (Battery <= 10.0)
+                    {
+                        Console.WriteLine("Low power");
+                    }
+                    if (Battery == 0.0)
+                    {
+                        canDrive = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No power");
+                Console.WriteLine("Cant drive!");
+                canDrive = false;
+            }
+            return canDrive;
+        }
+
+        //private methood to show the display, gets called inside the forloop
+        private void ShowDisplay(int distance)
+        {
+            Console.WriteLine($"||Battery on {Battery}% || driven {distance + 1} meters ||");
+        }
+
+        //methood to charge the car to 100%
+        public void Charge()
+        {
+            Battery = 100;
+        }
+
+    }
+}
